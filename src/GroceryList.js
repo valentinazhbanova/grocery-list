138a888f7be0480ffc8e74d93edce6f8229a74dc
valentinazhbanova <vala.ko@mail.ru>
2022-12-15 10:46:41 +0300

    first commit

---
+++ src/GroceryList.js
@@ -0,0 +1,70 @@
+import './App.css';
+import { Component } from 'react';
+import check from './check.jpg';
+
+export class GroceryList extends Component {
+    state = {
+        userInput: '',
+        groceryList: []
+    }
+
+    onChangeEvent(e) {
+        this.setState({userInput: e});
+    }
+    
+    addItem(input) {
+        if(input === '') {
+            alert('Please enter an item')
+        } else {
+        let listArray = this.state.groceryList;
+        listArray.push(input);
+        this.setState({groceryList: listArray, userInput: ''})
+    }
+    }
+
+    deleteItem() {
+        let listArray = this.state.groceryList;
+        listArray.length = 0;
+        this.setState({groceryList: listArray})
+        }
+
+    crossedWord(event) {
+        const li = event.target;
+        li.classList.toggle('crossed');
+    }
+
+onFormSubmit(e) {
+    e.preventDefault();
+}
+
+    render() {
+        return(
+            <div>
+                <form onSubmit={this.onFormSubmit}>
+                <div className='container'>
+                    <input type='text'
+                    placeholder='What to you want to buy today?'
+                    onChange={(e) => {this.onChangeEvent(e.target.value)}} 
+                    value={this.state.userInput} />
+                </div>
+
+                <div className='container'>
+                    <button onClick={() => this.addItem(this.state.userInput)} className='add'>Add</button>
+                </div>
+
+                <ul>
+                    {this.state.groceryList.map( (item, index) => (
+                        <li onClick={this.crossedWord} key={index}>
+                           <img src={check} width='40px' alt='purchases' />
+                            {item}</li>
+                    ))}
+                </ul>
+
+                <div className='container'>
+                <button onClick={() => this.deleteItem()} className='delete'>Delete</button>
+                </div>
+                </form>
+            </div>
+        )
+    }
+}
